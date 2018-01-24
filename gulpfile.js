2fc9a2264cfaa2024fa6b82b2e35456a7ed55af7
Daan Geerdink <d.geerdink@gmail.com>
2018-01-24 15:49:36 +0100

    js, index, scss

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,24 @@ let gulp = require('gulp');
 let rename = require('gulp-rename');
 let cleanCSS = require('gulp-clean-css');
 let sass = require('gulp-sass');
+let babel = require('gulp-babel');
+let concat = require('gulp-concat');
+let uglify = require('gulp-uglify');
+let sourcemaps = require('gulp-sourcemaps');
 
-const config = {
+let config = {
     paths: {
-        src:  './src',
-        dist: './dist'
+        src:       './src',
+        dist:      './dist',
+    },
+    scripts: {
+        lib: {
+            jquery: './node_modules/jquery/jquery.min.js',
+            popper: './node_modules/popper.js/dist/popper.min.js'
+        },
+        bootstrap: [
+            './node_modules/bootstrap/js/src/util.js'
+        ]
     }
 };
 
@@ -22,15 +35,40 @@ gulp.task('sass', function () {
 });
 
 gulp.task('sass:watch', function () {
-    gulp.watch(config.paths.src + '/sass/**/*.scss', ['sass']);
+    gulp.watch(config.paths.src + '/sass/**/*.scss', gulp.parallel('sass'));
 });
+
+gulp.task('scripts', function () {
+    return gulp.src([
+        config.paths.src + '/js/**/*.js',
+        config.scripts.lib.jquery,
+        config.scripts.lib.popper
+    ])
+        .pipe(sourcemaps.init())
+        .pipe(babel({ presets: ['env'] }))
+        .pipe(concat('scripts.js'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(config.paths.dist + '/js'))
+        .pipe(uglify())
+        .pipe(rename({ extname: '.min.js' }))
+        .pipe(gulp.dest(config.paths.dist + '/js'));
+});
+
+gulp.task('scripts:watch', function () {
+    gulp.watch(config.paths.src + '/sass/**/*.scss', gulp.parallel('sass'));
+});
+
+gulp.task('copyfiles', function () {
+    return gulp.src(config.paths.src + '/index.html')
+        .pipe(gulp.dest(config.paths.dist))
+})
 
 gulp.task('default', function (done) {
     gulp.parallel(
         'sass',
-        function() {
-
-    });
+        'scripts',
+        'copyfiles'
+    );
 
     done();
 });
