a7d25dc62e0e921cd5c3ca75b39580963213c941
Daan Geerdink <d.geerdink@gmail.com>
2018-01-24 17:38:42 +0100

    browsersync working

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 'use strict';
 
 let gulp = require('gulp');
+let del = require('del');
 let rename = require('gulp-rename');
 let postcss = require('gulp-postcss');
 let autoprefixer = require('autoprefixer');
@@ -10,6 +11,7 @@ let babel = require('gulp-babel');
 let concat = require('gulp-concat');
 let uglify = require('gulp-uglify');
 let sourcemaps = require('gulp-sourcemaps');
+let browserSync = require('browser-sync').create();
 
 let config = {
     paths: {
@@ -24,6 +26,10 @@ let config = {
     }
 };
 
+gulp.task('clean', function () {
+    return del(config.paths.dist);
+});
+
 gulp.task('sass', function () {
     return gulp.src(config.paths.src + '/sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
@@ -31,11 +37,15 @@ gulp.task('sass', function () {
         .pipe(gulp.dest(config.paths.dist + '/css'))
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(rename({ extname: '.min.css' }))
-        .pipe(gulp.dest(config.paths.dist + '/css'));
+        .pipe(gulp.dest(config.paths.dist + '/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
-gulp.task('sass:watch', function () {
-    gulp.watch(config.paths.src + '/sass/**/*.scss', gulp.parallel('sass'));
+gulp.task('sass:watch', function (done) {
+    gulp.watch(config.paths.src + '/sass/**/*.scss', gulp.series('sass'));
+    done();
 });
 
 gulp.task('scripts', function () {
@@ -52,17 +62,41 @@ gulp.task('scripts', function () {
         .pipe(gulp.dest(config.paths.dist + '/js'))
         .pipe(uglify())
         .pipe(rename({ extname: '.min.js' }))
-        .pipe(gulp.dest(config.paths.dist + '/js'));
+        .pipe(gulp.dest(config.paths.dist + '/js'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
-gulp.task('scripts:watch', function () {
-    gulp.watch(config.paths.src + '/sass/**/*.scss', gulp.parallel('sass'));
+gulp.task('scripts:watch', function (done) {
+    gulp.watch(config.paths.src + '/js/**/*.js', gulp.series('scripts'));
+    done();
 });
 
 gulp.task('copyfiles', function () {
     return gulp.src(config.paths.src + '/index.html')
         .pipe(gulp.dest(config.paths.dist))
-})
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task('files:watch', function (done) {
+    gulp.watch(config.paths.src + '/**/*.html', gulp.series('copyfiles'));
+    done();
+});
+
+gulp.task('watch', gulp.parallel('sass:watch', 'scripts:watch', 'files:watch'));
+
+gulp.task('browserSync', function () {
+    browserSync.init({
+        server: {
+            baseDir: './dist'
+        },
+    });
+});
+
+gulp.task('serve', gulp.series('clean', 'sass', 'scripts', 'copyfiles', gulp.parallel('watch', 'browserSync')));
 
 gulp.task('default', function (done) {
     gulp.parallel(
